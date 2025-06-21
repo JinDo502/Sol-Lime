e5961777564702f379fcded3d0b14cdf19e4ae1d
JinDo502 <x571839175@gmail.com>
2025-06-21 12:59:13 +0800

    WalletConnectWalletAdapter

--- src/components/Solana/SolanaProvider/index.tsx
+++ src/components/Solana/SolanaProvider/index.tsx
@@ -7,15 +7,13 @@ import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
-import { WalletConnectWalletAdapter } from '@solana/wallet-adapter-wallets';
-
+import { WalletConnectWalletAdapter } from '@walletconnect/solana-adapter';
 interface SolanaProviderProps {
   children: ReactNode;
 }
 
 export const SolanaProvider: FC<SolanaProviderProps> = ({ children }) => {
   const network = WalletAdapterNetwork.Devnet;
-
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
